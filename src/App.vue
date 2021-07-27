c643bc9e366e4b1d3cab1bc9648c2d05735163ec
Your Name <ikuto.hongu@gmail.com>
2021-07-27 12:47:25 +0900

    add dir

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <v-app id="inspire">
       <Header id="header"></Header>
       <v-main>
-        <router-view />
+        <Title></Title>
+        <About></About>
+        <Skill></Skill>
+        <Service></Service>
+        <Work></Work>
       </v-main>
     </v-app>
   </div>
@@ -11,12 +15,22 @@
 
 <script>
 import Header from './components/Header.vue'
+import Title from './components/Title.vue'
+import About from './components/About.vue'
+import Skill from './components/Skill.vue'
+import Work from './components/Work.vue'
+import Service from './components/Service.vue'
 
 export default {
   name: 'App',
 
   components: {
     Header,
+    Title,
+    About,
+    Skill,
+    Work,
+    Service
   },
   data() {
     return {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,18 @@
 <template>
   <div>
-      <Title></Title>
-      <About></About>
-      <Skill></Skill>
-      <Service></Service>
-      <Work></Work>
+
   </div>
 </template>
 
 <script>
 
-import Title from '../components/Title.vue'
-import About from '../components/About.vue'
-import Skill from '../components/Skill.vue'
-import Work from '../components/Work.vue'
-import Service from '../components/Service.vue'
+
 
 
 export default {
   name: 'home',
   components: {
-    Title,
-    About,
-    Skill,
-    Work,
-    Service
+
   }
 }
 </script>
